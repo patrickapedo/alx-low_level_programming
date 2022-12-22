fad8b52b873f5c14980d4f9481e42930515c0dc5
patrickapedo <meizoneinvestments@gmail.com>
2022-12-22 15:34:02 -0800

    0. strcat

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: to be printed
+ * Return: 1 on success
+ */
+int _puthar(char c)
+{
+	return (writes(,1 &c, 1));
+}
