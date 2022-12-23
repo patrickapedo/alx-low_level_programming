5be60947cd7af65f733f47c7377f946b2d699429
patrickapedo <meizoneinvestments@gmail.com>
2022-12-23 10:42:04 -0800

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,10 +12,10 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	i = 0
+	i = 0;
 	while (dest[i] != '\0')
 	{
-	i++;
+		i++;
 	}
 	j = 0;
 	while (j < n && src[j] != '\0')
@@ -24,6 +24,6 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 		j++;
 	}
-	dest[i] = '\0'
+	dest[i] = '\0';
 	return (dest);
 }
