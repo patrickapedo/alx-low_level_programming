b536d7265303e92a7231e6f9c02af9de5271664a
patrickapedo <meizoneinvestments@gmail.com>
2022-12-23 19:15:05 -0800

    8. rot13

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,35 +2,34 @@
 
 /**
  * cap_string - capitalizes words
- * @str: string
- * Return: pointer to string
+ * @str: The string to be capitalized.
+ * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int index = 0;
 
-	while (str[i])
+	while (str[index])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			i++;
-		if (str[i - 1] == ' ' ||
-		    str[i - 1] == '\t' ||
-		    str[i - 1] == '\n' ||
-		    str[i - 1] == ',' ||
-		    str[i - 1] == ';' ||
-		    str[i - 1] == '.' ||
-		    str[i - 1] == '!' ||
-		    str[i - 1] == '?' ||
-		    str[i - 1] == '"' ||
-		    str[i - 1] == '(' ||
-		    str[i - 1] == ')' ||
-		    str[i - 1] == '{' ||
-		    str[i - 1] == '}' ||
-		    i == 0)
-		       str[i] -= 32;
 
-	        i++;
+		if (str[index - 1] == ' ' ||
+	        str[index - 1] == '\t' ||
+                str[index - 1] == '\n' ||
+                str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+	        str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+	        str[index - 1] == '}' ||
+		index == 0)
+		        str[index] -= 32;
+	       index++;
 	}
-
 	return (str);
 }
